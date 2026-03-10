61d11c2aac40f7d7436015f3641ccf461c3a61ba
Cubek <manak47real@gmail.com>
2026-03-10 14:52:19 +0300

    new check

--- tests.sh
+++ tests.sh
@@ -2,12 +2,11 @@
 cd "$(dirname "$0")"
 
 test=1
-set -o pipefail
 
 for i in $(ls example); do
   target/release/bengal example/$i > /dev/null || { test=0; echo -e "\e[31m$i finished with error\e[0m"; }
 done
 
 if [ $test == 0 ]; then
-  exit
+  trap 'echo "Some tests failed" > &2' ERR
 fi
